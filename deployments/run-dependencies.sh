f72059e9e77e4f37e6b796ad8bc7efad9e180914
M Cholis Malik <cholis@code.id>
2021-07-19 06:47:01 +0700

    split script for sequence deployments

--- deployments/run-dependencies.sh
+++ deployments/run-dependencies.sh
@@ -1,6 +1,6 @@
 # run elasticsearch
 sudo sysctl -w vm.max_map_count=262144
-docker compose -f elasticsearch/docker-compose.yml up -d
+docker compose -f dependencies/elasticsearch/docker-compose.yml up -d
 
 # run postgres
-docker compose -f postgres/docker-compose.yml up -d
+docker compose -f dependencies/postgres/docker-compose.yml up -d
